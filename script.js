51cf4fd29156992851ba3d41a84480b35287906b
Matthew Baka <matthewbaka@gmail.com>
2023-09-13 00:56:38 -0400

    added ideas

--- script.js
+++ script.js
@@ -1,3 +1,115 @@
 /*
 
+home screen
+    high scores
+    start button goes to quiz screen
+
+quiz screen
+    question
+    answers
+    time
+    submit button gives
+        correct or incorrect answer
+        next button
+
+results screen
+    save inititals
+    high score
+    submit button goes to home screen
+
+120 seconds
+wrong answer is minus 10
+
+Which of the following is a programming lanuage?
+Java (the script)
+Java (the coffee)
+Java (the island)
+Jabba (The Hut)
+
+What does the % sign mean?
+percentage
+modulus
+exponentation
+skateboarding
+
+What is a string in Javascript?
+a line of code
+data stored as text
+a mathematical expression
+material twisted of thin threads
+
+What tells Javascript to return true if one of two options are true?
+\\
+||
+//
+¯\_(ツ)_/¯
+
+Which of the following is not a data type?
+object
+boolean
+function
+glockenspiel
+
+Which of the following can not define the ends of a string?
+"
+'
+`
+麤
+
+What does 1+1+"1" evaluate to?
+3
+111
+21
+9000
+
+What does "1"+"1"-1 evaluate to?
+
+What does x-- do?
+negates x
+subtracts 1 from x
+converts the value of x to binary
+crashes your browser
+
+How do you tell Javascript to take an exponent?
+^
+**
+exp()
+speak firmly yet politely to the monitor
+
+Which of the following would not appear in a for loop?
+var i = 0
+i < 10
+i++
+i love you
+
+What does the .push() method do?
+adds an element to the start of an array
+adds an element to the end of an array
+removes an element from the end of an array
+shoves the user off their chair
+
+What method adds an element to the start of an array?
+.add()
+.shift()
+.append()
+.putItRightAtTheStart()
+
+Which of the following keywords does not declare a variable?
+let
+var
+const
+mango
+
+What is the colon (:) used for in Javascript?
+To start a for loop
+To separate values in an array
+To assign values to a key in an object
+Absorbs water and electrolytes from undigested food
+
+What is the semicolon (;) used for in Javascript?
+To close an if statement
+To take the modulus of a number
+To end a declaration, initialization, function
+Absorbs water and electrolytes from semi-undigested food
+
 */
