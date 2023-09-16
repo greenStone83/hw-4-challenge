5b7215fbf3febcc6035dfb96c269d9bd4737a34f
Matthew Baka <matthewbaka@gmail.com>
2023-09-16 18:11:11 -0400

    added every question

---
+++ questions.js
@@ -0,0 +1,223 @@
+//list of questions and answers
+let questions = [
+    {
+        question: 'Which of the following is a programming lanuage?',
+        answers: [
+            'Java (the script)',
+            'Java (the coffee)',
+            'Java (the island)',
+            'Jabba (The Hut)',
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: 'What does the % sign mean?',
+        answers: [
+            'percentage',
+            'modulus',
+            'exponentation',
+            'skateboarding',
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: 'What is a string?',
+        answers: [
+            'a line of code',
+            'data stored as text',
+            'a mathematical expression',
+            'material twisted of thin threads',
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: 'Which of the following keywords does not declare a variable?',
+        answers: [
+            'let',
+            'var',
+            'const',
+            'mango',
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: 'What does x-- do?',
+        answers: [
+            'negates x',
+            'subtracts 1 from x',
+            'converts the value of x to binary',
+            'crashes your browser',
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: 'What is the correct way to say "else if" in Javascript?',
+        answers: [
+            'else if',
+            'elsif',
+            'elif',
+            'elsa',
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: 'What is a global variable?',
+        answers: [
+            'A variable that overflows or underflows',
+            'A variable that makes reference to itself',
+            'A variable that can be accessed from everywhere in the code',
+            'A variable that has connections in multiple countries',
+        ],
+        correctAnswer: 3,
+    },
+    {
+        question: 'Which of the following is not a data type?',
+        answers: [
+            'object',
+            'boolean',
+            'function',
+            'glockenspiel',
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: 'What does the .push() method do?',
+        answers: [
+            'adds an element to the start of an array',
+            'adds an element to the end of an array',
+            'removes an element from the end of an array',
+            'shoves the user off their chair',
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: 'What method adds an element to the start of an array?',
+        answers: [
+            '.add()',
+            '.shift()',
+            '.append()',
+            '.putItRightAtTheStart()',
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: 'What checks if two values are equal in type and value?',
+        answers: [
+            '=',
+            '==',
+            '===',
+            '============',
+        ],
+        correctAnswer: 3,
+    },
+    {
+        question: 'Which of the following can not define the ends of a string?',
+        answers: [
+            '"',
+            "'",
+            '`',
+            '有',
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: 'How do you tell Javascript to take an exponent?',
+        answers: [
+            '^',
+            '**',
+            'exp()',
+            'speak firmly yet politely to the monitor',
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: 'How do you get the length of an array?',
+        answers: [
+            'array.length',
+            'array.len',
+            'length(array)',
+            'howLongisTheArrayPlease(array)',
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: 'What tells Javascript to return true if one of two options are true?',
+        answers: [
+            '\\\\',
+            '||',
+            '//',
+            '¯\\_(ツ)_/¯',
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: 'Which of the following would not appear in a for loop?',
+        answers: [
+            'var i = 0',
+            'i < 10',
+            'i++',
+            'i love you',
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: 'What does the keyword break do?',
+        answers: [
+            'exits a loop',
+            'closes the text file',
+            'ends the program',
+            'overheats your CPU',
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: 'What does 1+1+"1" evaluate to?',
+        answers: [
+            '3',
+            '111',
+            'NaN',
+            '21',
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: 'What does "1"+"1"-1 evaluate to?',
+        answers: [
+            '1 (number)',
+            '1 (string)',
+            'NaN',
+            '20',
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: 'Which is not a way to define a function?',
+        answers: [
+            'runCode = function()',
+            'function runCode()',
+            'runCode = () =>',
+            '😎😎runCode😎😎',
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: 'What is the colon (:) used for?',
+        answers: [
+            'To start a for loop',
+            'To separate values in an array',
+            'To assign values to a key in an object',
+            'To absorb water and electrolytes from undigested food',
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: 'What is the semicolon (;) used for?',
+        answers: [
+            'To close an if statement',
+            'To take the modulus of a number',
+            'To end a declaration, initialization, function',
+            'To absorb water and electrolytes from semi-undigested food',
+        ],
+        correctAnswer: 4,
+    },
+];
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const quizTime = 40;
+const quizTime = 200;
 const timePenalty = 20;
 
 let questionNumber = 0;
@@ -30,40 +30,6 @@ let record = document.querySelector('#record');
 let instruct = document.querySelector('#instruct');
 let input = document.querySelector('#input');
 let save = document.querySelector('#save');
-
-//list of questions and answers
-let questions = [
-    {
-        question: 'Question 1',
-        answers: [
-            'answer 1',
-            'answer 2',
-            'answer 3',
-            'answer 4',
-        ],
-        correctAnswer: 1,
-    },
-    {
-        question: 'Question 2',
-        answers: [
-            'answer 1',
-            'answer 2',
-            'answer 3',
-            'answer 4',
-        ],
-        correctAnswer: 1,
-    },
-    {
-        question: 'Question 3',
-        answers: [
-            'answer 1',
-            'answer 2',
-            'answer 3',
-            'answer 4',
-        ],
-        correctAnswer: 1,
-    },
-];
 
 //start quiz
 start.addEventListener('click', () => {
@@ -131,7 +97,7 @@ submit.addEventListener('click', () => {
             submit.innerText = 'Next';
             //right answer turns green
             buttons[questions[questionNumber].correctAnswer - 1].setAttribute('style', 'background-color: lightgreen;');
-            //if wrong answer chosen, answer turns pink and time decreases
+            //if wrong answer chosen, answer turns pink
             if (selectedAnswer !== questions[questionNumber].correctAnswer) {
                 buttons[selectedAnswer - 1].setAttribute('style', 'background-color: pink;');
                 //deduct time if answered wrong
@@ -208,105 +174,3 @@ save.addEventListener('click', () => {
         reset();
     }
 });
-
-/*
-
-Which of the following is a programming lanuage?
-Java (the script)
-Java (the coffee)
-Java (the island)
-Jabba (The Hut)
-
-What does the % sign mean?
-percentage
-modulus
-exponentation
-skateboarding
-
-What is a string in Javascript?
-a line of code
-data stored as text
-a mathematical expression
-material twisted of thin threads
-
-What tells Javascript to return true if one of two options are true?
-\\
-||
-//
-¯\_(ツ)_/¯
-
-Which of the following is not a data type?
-object
-boolean
-function
-glockenspiel
-
-Which of the following can not define the ends of a string?
-"
-'
-`
-麤
-
-What does 1+1+"1" evaluate to?
-3
-111
-21
-9000
-
-What does "1"+"1"-1 evaluate to?
-
-What does x-- do?
-negates x
-subtracts 1 from x
-converts the value of x to binary
-crashes your browser
-
-How do you tell Javascript to take an exponent?
-^
-**
-exp()
-speak firmly yet politely to the monitor
-
-Which of the following would not appear in a for loop?
-var i = 0
-i < 10
-i++
-i love you
-
-What does the .push() method do?
-adds an element to the start of an array
-adds an element to the end of an array
-removes an element from the end of an array
-shoves the user off their chair
-
-What method adds an element to the start of an array?
-.add()
-.shift()
-.append()
-.putItRightAtTheStart()
-
-Which of the following keywords does not declare a variable?
-let
-var
-const
-mango
-
-What is the correct way to say "ekse if" in Javascript?
-else if
-elsif
-elif
-elsa
-
-What is the colon (:) used for in Javascript?
-To start a for loop
-To separate values in an array
-To assign values to a key in an object
-Absorbs water and electrolytes from undigested food
-
-What is the semicolon (;) used for in Javascript?
-To close an if statement
-To take the modulus of a number
-To end a declaration, initialization, function
-Absorbs water and electrolytes from semi-undigested food
-
-*/
